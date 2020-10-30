27f7f9e0f532b6eb737f7037bd837b76440dce17
SagarDeoghare <deoghare.sagar2010@gmail.com>
2020-10-30 23:37:47 +0530

    Init website'

---
+++ studentregistration/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    {{msg}}
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+
+  }),
+
+  props: {
+    msg: ''
+  }
+};
+</script>
